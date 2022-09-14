fc84c7b39db17626682fbac46e999614a1d18b42
Germán Vanni <germanvanni1@gmail.com>
2022-09-14 15:54:21 -0300

    8/9

--- app/src/main/java/com/example/banco_utn_gts/MainActivity.java
+++ app/src/main/java/com/example/banco_utn_gts/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.banco_utn_gts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import com.example.banco_utn_gts.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<String> listaDeMonedas = new ArrayList<String>();
+        listaDeMonedas.add("PESOS");
+        listaDeMonedas.add("DOLAR");
+
+        //Spinner spinner = findViewById();
     }
 }
---
+++ app/src/main/java/com/example/banco_utn_gts/SimularPlazoFijo.java
@@ -0,0 +1,14 @@
+package com.example.banco_utn_gts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimularPlazoFijo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simular_plazo_fijo);
+    }
+}

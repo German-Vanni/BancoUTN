e5513e0f7584ecaec7aabef5c109c57d31d0ceb7
Germán Vanni <germanvanni1@gmail.com>
2022-09-28 17:24:28 -0300

    Actualizacion dinamica de los campos en funcion de la entrada, Navegacion con Bundle, Mensaje de creacion de plazo

--- app/src/main/java/com/example/banco_utn_gts/MainActivity.java
+++ app/src/main/java/com/example/banco_utn_gts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.banco_utn_gts;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    Boolean simulado = false;
+    int diasSimulados = -1;
+    Double capitalInvertirSimulado = -1.;
+    String nombre = "";
+    String apellido = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,50 @@ public class MainActivity extends AppCompatActivity {
         List<String> listaDeMonedas = new ArrayList<String>();
         listaDeMonedas.add("PESOS");
         listaDeMonedas.add("DOLAR");
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,listaDeMonedas);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listaDeMonedas);
 
         binding.spinnerMoneda.setAdapter(adapter);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            if (bundle.containsKey("plazo") && bundle.containsKey("capitalInvertir")) {
+                diasSimulados = bundle.getInt("plazo");
+                capitalInvertirSimulado = bundle.getDouble("capitalInvertir");
+                simulado = true;
+                binding.constituirButton.setEnabled(true);
+            }
+        }
+
+
         binding.simularButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SimularPlazoFijo.class);
                 startActivity(intent);
+            }
+        });
+        binding.constituirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Validar
+                String capitalString = String.format("%.2f",capitalInvertirSimulado);
+                String message = "Tu plazo fijo de ";
+                message += capitalString + " por " + diasSimulados + " días ha sido constituido!";
+
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder
+                        .setTitle("Felicitaciones " + nombre + " " + apellido + "!")
+                        .setMessage(message)
+                        .setPositiveButton("PIOLA", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
         });
     }
--- app/src/main/java/com/example/banco_utn_gts/SimularPlazoFijo.java
+++ app/src/main/java/com/example/banco_utn_gts/SimularPlazoFijo.java
@@ -2,9 +2,13 @@ package com.example.banco_utn_gts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.banco_utn_gts.databinding.ActivitySimularPlazoFijoBinding;
 
@@ -13,35 +17,115 @@ public class SimularPlazoFijo extends AppCompatActivity {
     private double tasaNomAnual = -1;
     private double tasaEfecAnual = -1;
     private double capitalInvertir  = -1;
+    private int plazo = 30;
+
+    TextView plazoResultTextView;
+    TextView capitalResultTextView;
+    TextView interesesResultTextView;
+    TextView montoTotalResultTextView;
+    TextView montoTotalAnualResultTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySimularPlazoFijoBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());;
+        setContentView(binding.getRoot());
 
-        binding.tasaNominalAnualEditText.addTextChangedListener(new TextWatcher() {
+        //TEST
+//        binding.capitalInvertirEditText.setText("100000");
+//        binding.tasaNominalAnualEditText.setText("69.5");
+//        binding.tasaEfectivaAnualEditText.setText("96.33");
+
+        binding.confirmarButton.setEnabled(false);
+
+        binding.tasaNominalAnualEditText.addTextChangedListener(new InputValuesTextWatcher());
+        binding.tasaEfectivaAnualEditText.addTextChangedListener(new InputValuesTextWatcher());
+        binding.capitalInvertirEditText.addTextChangedListener(new InputValuesTextWatcher());
+        binding.plazoSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                plazo = seekBar.getProgress()*30;
+                String plazoString = " " + plazo  + " Días";
+                plazoResultTextView.setText(plazoString);
+                binding.seekbarLabelTextView.setText(plazoString);
                 if(CheckValidValuesOnInput()){
-                    binding.confirmarButton.setEnabled(true);
-                }
-                else{
-                    binding.confirmarButton.setSelected(false);
-                    binding.confirmarButton.setEnabled(false);
+                    calcular();
                 }
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
+
+        //Creamos los views necesarios para colocar los valores calculados dinamicamente
+        // en la parte inferior de la pantalla
+        plazoResultTextView = new TextView(getApplicationContext());
+        capitalResultTextView = new TextView(getApplicationContext());
+        interesesResultTextView = new TextView(getApplicationContext());
+        montoTotalResultTextView = new TextView(getApplicationContext());
+        montoTotalAnualResultTextView = new TextView(getApplicationContext());
+
+
+        String plazoString = " 3 Días";
+        plazoResultTextView.setText(plazoString);
+        binding.resultadoPlazoLinearlayout.addView(plazoResultTextView);
+        binding.resultadoCapitalLinearlayout.addView(capitalResultTextView);
+        binding.resultadoInteresesGanadosLinearlayout.addView(interesesResultTextView);
+        binding.resultadoMontoTotalLinearlayout.addView(montoTotalResultTextView);
+        binding.resultadoMontoTotalAnualLinearlayout.addView(montoTotalAnualResultTextView);
+
+        binding.confirmarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SimularPlazoFijo.this, MainActivity.class);
+                i.putExtra("capitalInvertir", capitalInvertir);
+                i.putExtra("plazo", plazo);
+                startActivity(i);
+            }
+        });
+    }
+
+
+    private class InputValuesTextWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if(CheckValidValuesOnInput()){
+                binding.confirmarButton.setEnabled(true);
+                calcular();
+            }
+            else{
+//              binding.confirmarButton.setSelected(false);
+                binding.confirmarButton.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    }
+
+    private void calcular() {
+        capitalResultTextView.setText(binding.capitalInvertirEditText.getText());
+        Double interesesGanados = capitalInvertir * (tasaNomAnual / 100) * plazo / (360) ;
+        interesesResultTextView.setText(String.format("%.2f", interesesGanados));
+        Double montoTotal = capitalInvertir + interesesGanados;
+        montoTotalResultTextView.setText(String.format("%.2f",montoTotal));
+        Double montoTotalAnual = (capitalInvertir * (tasaNomAnual / 100)) + capitalInvertir;
+        montoTotalAnualResultTextView.setText(String.format("%.2f", montoTotalAnual));
     }
 
     protected Boolean CheckValidValuesOnInput(){
@@ -75,6 +159,5 @@ public class SimularPlazoFijo extends AppCompatActivity {
 
         return true;
     }
-
 
 }
